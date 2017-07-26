69c7674259c2a6e98fafa4f0f468d8a65caa0446
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-07-26 18:40:39 +0300

    finish example project

--- DragAndDropCellsExample/ViewController.swift
+++ DragAndDropCellsExample/ViewController.swift
@@ -8,18 +8,204 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var items = [[String]]()
+    
+    required init(coder aDecoder: NSCoder) {
+        var itemsArray = [String]()
+        var itemsArray2 = [String]()
+        
+        let item1 = "One"
+        let item2 = "Two"
+        let item3 = "Three"
+        
+        itemsArray.append(item1)
+        itemsArray.append(item2)
+        itemsArray.append(item3)
+        
+        let item21 = "1"
+        let item22 = "2"
+        let item23 = "3"
+        
+        itemsArray2.append(item21)
+        itemsArray2.append(item22)
+        itemsArray2.append(item23)
+        
+        items.append(itemsArray)
+        items.append(itemsArray2)
+        
+        super.init(coder: aDecoder)!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
+        
+        let longpress = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longPressGestureRecognized(_:)))
+        tableView.addGestureRecognizer(longpress)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func longPressGestureRecognized(_ gestureRecognizer: UIGestureRecognizer) {
+        let longPress = gestureRecognizer as! UILongPressGestureRecognizer
+        let state = longPress.state
+        let locationInView = longPress.location(in: tableView)
+        let indexPath = tableView.indexPathForRow(at: locationInView)
+        
+        struct My {
+            static var cellSnapshot : UIView? = nil
+            static var cellIsAnimating : Bool = false
+            static var cellNeedToShow : Bool = false
+        }
+        struct Path {
+            static var initialIndexPath : IndexPath? = nil
+        }
+        
+        switch state {
+        case UIGestureRecognizerState.began:
+            if indexPath != nil {
+                Path.initialIndexPath = indexPath
+                let cell = tableView.cellForRow(at: indexPath!) as UITableViewCell!
+                My.cellSnapshot  = snapshotOfCell(cell!)
+                
+                var center = cell?.center
+                My.cellSnapshot!.center = center!
+                My.cellSnapshot!.alpha = 0.0
+                tableView.addSubview(My.cellSnapshot!)
+                
+                UIView.animate(withDuration: 0.25, animations: { () -> Void in
+                    center?.y = locationInView.y
+                    My.cellIsAnimating = true
+                    My.cellSnapshot!.center = center!
+                    My.cellSnapshot!.transform = CGAffineTransform(scaleX: 1.05, y: 1.05)
+                    My.cellSnapshot!.alpha = 0.98
+                    cell?.alpha = 0.0
+                }, completion: { (finished) -> Void in
+                    if finished {
+                        My.cellIsAnimating = false
+                        if My.cellNeedToShow {
+                            My.cellNeedToShow = false
+                            UIView.animate(withDuration: 0.25, animations: { () -> Void in
+                                cell?.alpha = 1
+                            })
+                        } else {
+                            cell?.isHidden = true
+                        }
+                    }
+                })
+            }
+            
+        case UIGestureRecognizerState.changed:
+            if My.cellSnapshot != nil {
+                var center = My.cellSnapshot!.center
+                center.y = locationInView.y
+                My.cellSnapshot!.center = center
+                
+                if ((indexPath != nil) && (indexPath != Path.initialIndexPath)) {
+                    
+                    let sourceItem = items[(Path.initialIndexPath?.section)!][(Path.initialIndexPath?.row)!]
+                    
+                    if Path.initialIndexPath?.section == indexPath?.section {
+                        
+                        swap(&items[(Path.initialIndexPath?.section)!][(Path.initialIndexPath?.row)!], &items[(Path.initialIndexPath?.section)!][(indexPath?.row)!])
+                        
+                    } else {
+                        
+                        items[(Path.initialIndexPath?.section)!] = items[(Path.initialIndexPath?.section)!].filter{$0 != sourceItem}
+                        
+                        items[(indexPath?.section)!].insert(sourceItem, at: (indexPath?.row)!)
+                    }
+                    print("first section has \(items[0].count) rows\nsecond section has \(items[1].count) rows")
+                    tableView.reloadData()
+                    Path.initialIndexPath = indexPath
+                    
+                }
+            }
+        default:
+            if Path.initialIndexPath != nil {
+                let cell = tableView.cellForRow(at: Path.initialIndexPath!) as UITableViewCell!
+                if My.cellIsAnimating {
+                    My.cellNeedToShow = true
+                } else {
+                    cell?.isHidden = false
+                    cell?.alpha = 0.0
+                }
+                
+                UIView.animate(withDuration: 0.25, animations: { () -> Void in
+                    My.cellSnapshot!.center = (cell?.center)!
+                    My.cellSnapshot!.transform = CGAffineTransform.identity
+                    My.cellSnapshot!.alpha = 0.0
+                    cell?.alpha = 1.0
+                    
+                }, completion: { (finished) -> Void in
+                    if finished {
+                        Path.initialIndexPath = nil
+                        My.cellSnapshot!.removeFromSuperview()
+                        My.cellSnapshot = nil
+                    }
+                })
+            }
+        }
+    }
+    
+    func snapshotOfCell(_ inputView: UIView) -> UIView {
+        UIGraphicsBeginImageContextWithOptions(inputView.bounds.size, false, 0.0)
+        inputView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        let cellSnapshot : UIView = UIImageView(image: image)
+        cellSnapshot.layer.masksToBounds = false
+        cellSnapshot.layer.cornerRadius = 0.0
+        cellSnapshot.layer.shadowOffset = CGSize(width: -5.0, height: 0.0)
+        cellSnapshot.layer.shadowRadius = 5.0
+        cellSnapshot.layer.shadowOpacity = 0.4
+        return cellSnapshot
+    }
+    
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if !items[section].isEmpty {
+            let header = UITableViewHeaderFooterView()
+            header.textLabel?.text = "section # \(section + 1)"
+            header.textLabel?.textColor = UIColor.black
+            header.tintColor = UIColor.red
+            return header
+        }
+        return nil
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if !items[section].isEmpty {
+            return 44.0
+        } else {
+            return 0.0
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = items[indexPath.section][indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
 }
+
 
