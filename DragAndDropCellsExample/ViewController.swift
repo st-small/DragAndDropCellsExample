8f1b1f65534a2c77a8c1a507bf45495a8eec512e
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-07-26 18:33:15 +0300

    Initial Commit

---
+++ DragAndDropCellsExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DragAndDropCellsExample
+//
+//  Created by Stanly Shiyanovskiy on 26.07.17.
+//  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
